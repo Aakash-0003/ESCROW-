d97830e418ee8f3992a80710bf6add1a880361d4
Aakash_03 <aakashtomer1709@gmail.com>
2024-11-19 10:01:42 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545", // Default Hardhat Network or Ganache URL
+      url: "http://127.0.0.1:8545", // Default Hardhat Network
     },
     sepolia: {
       url: process.env.RPC_URL,
